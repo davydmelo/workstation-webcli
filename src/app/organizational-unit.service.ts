4f12915ef8bdfd94efcd168affcd4c8bc25c7fb0
Davyd Melo <davydmelo@gmail.com>
2017-04-09 22:48:46 -0300

    Estilização do menu principal e lista de unidades organizacionais.

--- src/app/organizational-unit.service.ts
+++ src/app/organizational-unit.service.ts
@@ -6,7 +6,10 @@ export class OrganizationalUnitService {
 
   ous: OrganizationalUnit[] = [
     new OrganizationalUnit('Sala 1', 1),
-    new OrganizationalUnit('Sala 2', 2)
+    new OrganizationalUnit('Sala 2', 2),
+    new OrganizationalUnit('Sala 3', 3),
+    new OrganizationalUnit('Sala 4', 4),
+    new OrganizationalUnit('Sala 5', 5)
   ];
 
   constructor() { }
